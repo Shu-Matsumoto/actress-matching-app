d0eb9a4f71322ef728a7f5557a1f7603451443d8
Shu-Matsumoto <103585072+Shu-Matsumoto@users.noreply.github.com>
2022-08-25 17:49:11 +0900

    pagesのサンプルコード実装

---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,11 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Object>,
+) {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users/')
+  const users = await response.json()
+  res.status(200).json({ users })
+}
